bfd08fcd5237f82495c8ba128cc54b70c6d393db
pol <pol.renau@est.fib.upc.edu>
2020-09-21 13:57:47 +0200

    mesage

---
+++ swarm_webserver_nginx_no_expose_ports/Container1/Api.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+from json import loads
+app = Flask(__name__)
+
+@app.route('/')
+def generate_invoice_df():
+    x =  '{ "name":"John", "age":30, "city":"New York"}'
+
+    # parse x:
+    y = loads(x)
+
+    return y
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port='5000')
